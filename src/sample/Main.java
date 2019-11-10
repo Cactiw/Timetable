e2eb299843f5caf3a589da28cfad69471b246092
Cactiw <Knyazev1514@gmail.com>
2019-11-10 23:05:25 +0300

    Sidebar added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
@@ -27,9 +29,32 @@ public class Main extends Application{
         StackPane main_layout = new StackPane();
         main_layout.getChildren().add(button);
 
+        BorderPane root_pane = new BorderPane();
+        root_pane.setLeft(sidePane());
+
         Scene scene = new Scene(main_layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private VBox sidePane() {
+        VBox vbox = new VBox();
+        vbox.setPrefWidth(500);
+        vbox.setStyle("-fx-background-color: #594031");
+        vbox.getChildren().add(boxItem("delete"));
+        return vbox;
+    }
+
+    private HBox boxItem(String item) {
+        Image image = new Image(JavaFXMenu.class.getResource("/icons/" + item + ".png").toExternalForm());
+        ImageView view = new ImageView(image);
+        Button button = new Button();
+        button.setGraphic(view);
+        Pane paneIndicator = new Pane();
+        paneIndicator.setPrefSize(100, 500);
+        HBox box = new HBox(button);
+        box.setStyle("-fx-background-color: #594031");
+        return box;
     }
 
 
