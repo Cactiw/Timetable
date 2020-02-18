30d62640b57a61954589c7a38f9eaaabbf8e8bec
Cactiw <Knyazev1514@gmail.com>
2020-02-18 16:30:00 +0300

    UserRepository refactor

--- src/main/java/Timetable/repositories/UserRepository.java
+++ src/main/java/Timetable/repositories/UserRepository.java
@@ -1,6 +1,6 @@
 package Timetable.repositories;
 
-import Timetable.classes.User;
+import Timetable.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
