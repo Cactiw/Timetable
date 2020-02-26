025c53605703e0bf121a471b4824c601aa8f0eb3
Cactiw <Knyazev1514@gmail.com>
2020-02-26 10:59:38 +0300

    Added splash loading screen at startup

--- src/main/java/Timetable/AbstractJavaFxApplicationSupport.java
+++ src/main/java/Timetable/AbstractJavaFxApplicationSupport.java
@@ -1,19 +1,54 @@
 package Timetable;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
 
 public abstract class AbstractJavaFxApplicationSupport extends Application {
 
     private static String[] savedArgs;
+    private Stage splashScreen;
 
     protected ConfigurableApplicationContext context;
 
     @Override
     public void init() throws Exception {
+        Platform.runLater(this::showSplash);
         context = SpringApplication.run(getClass(), savedArgs);
         context.getAutowireCapableBeanFactory().autowireBean(this);
+        Platform.runLater(this::closeSplash);
+    }
+
+    /**
+     * Загружаем заставку обычным способом. Выставляем везде прозрачность
+     */
+    private void showSplash() {
+        try {
+            splashScreen = new Stage(StageStyle.TRANSPARENT);
+            splashScreen.setTitle("Splash");
+            Parent root = FXMLLoader.load(getClass().getResource("../splash.fxml"));
+            Scene scene = new Scene(root, Color.TRANSPARENT);
+            splashScreen.setScene(scene);
+            splashScreen.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Закрывает сцену с заставкой
+     */
+    private void closeSplash() {
+        splashScreen.close();
     }
 
     @Override
