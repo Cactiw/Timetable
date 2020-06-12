badba60580db98f33e57f0d1236aac3a50e66ef6
Cactiw <Knyazev1514@gmail.com>
2020-06-12 14:25:19 +0300

    Fixed getDefaultWeek method

--- src/main/java/Timetable/repositories/PairRepository.java
+++ src/main/java/Timetable/repositories/PairRepository.java
@@ -18,7 +18,8 @@ public interface PairRepository extends JpaRepository<Pair, Integer> {
     @Query("select p from Pair p where p.auditorium = ?1 and (p.beginTime < ?3 and p.endTime > ?2)")
     List<Pair> getAllAuditoriumConflicts(Auditorium auditorium, LocalDateTime beginTime, LocalDateTime endTime);
 
-    @Query("select p from Pair p where p.repeatability > 0 order by p.dayOfTheWeek asc, p.clearEndTIme asc")
+    @Query("select p from Pair p where p.repeatability > 0 and p.group in ?1 order by p.dayOfTheWeek asc, " +
+            "p.clearEndTIme asc")
     List<Pair> getGroupsDefaultWeek(List<PeopleUnion> peopleUnions);
 
     List<Pair> getAllByGroupInAndRepeatabilityEqualsOrderByEndTimeAsc(List<PeopleUnion> peopleUnions, int repeatability);
