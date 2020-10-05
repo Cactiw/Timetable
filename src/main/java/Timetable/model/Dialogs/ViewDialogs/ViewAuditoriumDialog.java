c7c2285acf1ffdb791ed26c6aae3d1f9d6311d96
Cactiw <Knyazev1514@gmail.com>
2020-10-05 17:43:30 +0300

    Added auditorium pairs

--- src/main/java/Timetable/model/Dialogs/ViewDialogs/ViewAuditoriumDialog.java
+++ src/main/java/Timetable/model/Dialogs/ViewDialogs/ViewAuditoriumDialog.java
@@ -4,21 +4,30 @@ import Timetable.model.Auditorium;
 import Timetable.model.AuditoriumProperty;
 import Timetable.model.Dialogs.AddDialogs.AddAuditoriumDialog;
 import Timetable.model.Dialogs.DeleteDialogs.DeleteAuditoriumDialog;
+import Timetable.model.Pair;
+import Timetable.service.DateService;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 @Component
 public class ViewAuditoriumDialog {
@@ -44,6 +53,8 @@ public class ViewAuditoriumDialog {
         dialog = new JFXDialog();
         dialog.setTransitionType(JFXDialog.DialogTransition.CENTER);
         content = new JFXDialogLayout();
+        content.prefWidthProperty().bind(dialog.widthProperty());
+        dialog.setPadding(new Insets(0, 250, 0, 50));
 
         updateContent();
 
@@ -61,46 +72,96 @@ public class ViewAuditoriumDialog {
         okButton.setOnAction(e -> this.dialog.close());
 
         final GridPane rootPane = new GridPane();
+        final GridPane infoPane = new GridPane();
 
         final Text heading = new Text(auditorium.getName());
         heading.styleProperty().setValue("-fx-font-size: 14pt;");
         content.setHeading(heading);
         content.setBody(rootPane);
 
-        rootPane.setPadding(new Insets(10, 0, 10, 0));
-        rootPane.setHgap(5);
-        rootPane.prefWidthProperty().bind(dialog.widthProperty());
+        infoPane.setPadding(new Insets(10, 0, 10, 0));
+        infoPane.setHgap(5);
+        infoPane.prefWidthProperty().bind(dialog.widthProperty());
 
         final Image editIcon = new Image("/icons/edit.png");
-        final JFXButton editPairButton = new JFXButton("", new ImageView(editIcon));
-        editPairButton.setOnAction(e -> {
+        final JFXButton editAuditoriumButton = new JFXButton("", new ImageView(editIcon));
+        editAuditoriumButton.setOnAction(e -> {
             addAuditoriumDialog.showFromAuditorium(auditorium);
             this.updateContent();
         });
-        rootPane.add(editPairButton, 3, 0);
+        infoPane.add(editAuditoriumButton, 3, 0);
 
         final Image deleteIcon = new Image("/icons/delete.png");
-        final JFXButton deletePairButton = new JFXButton("", new ImageView(deleteIcon));
-        deletePairButton.setOnMouseClicked(e -> {
+        final JFXButton deleteAuditoriumButton = new JFXButton("", new ImageView(deleteIcon));
+        deleteAuditoriumButton.setOnMouseClicked(e -> {
             deleteAuditoriumDialog.show(container, auditorium);
         });
-        rootPane.add(deletePairButton, 4, 0);
+        infoPane.add(deleteAuditoriumButton, 4, 0);
 
-        rootPane.add(new Label("Название:"), 0, 0);
-        rootPane.add(new Label(auditorium.getName()), 1, 0, 2, 1);
+        infoPane.add(new Label("Название:"), 0, 0);
+        infoPane.add(new Label(auditorium.getName()), 1, 0, 2, 1);
 
-        rootPane.add(new Label("Вместимость:"), 0, 1);
-        rootPane.add(new Label(auditorium.getMaxStudents().toString()), 1, 1, 2, 1);
+        infoPane.add(new Label("Вместимость:"), 0, 1);
+        infoPane.add(new Label(auditorium.getMaxStudents().toString()), 1, 1, 2, 1);
         int rowIndex = 2;
 
         if (!auditorium.getProperties().isEmpty()) {
-            rootPane.add(new Separator(), 0, rowIndex, 2, 1);
+            infoPane.add(new Separator(), 0, rowIndex, 2, 1);
             rowIndex += 1;
             for (AuditoriumProperty property: auditorium.getProperties()) {
-                rootPane.add(new Label(property.getName()), 0, rowIndex, 2, 1);
+                infoPane.add(new Label(property.getName()), 0, rowIndex, 2, 1);
                 rowIndex += 1;
             }
         }
+        VBox auditoriumPairs = getAuditoriumPairs();
+        auditoriumPairs.prefWidthProperty().bind(dialog.widthProperty());
+
+        rootPane.add(infoPane, 0, 0);
+        rootPane.add(auditoriumPairs, 1, 0);
+    }
+
+    @NonNull
+    VBox getAuditoriumPairs() {
+        VBox root = new VBox();
+        root.setSpacing(10);
+        Label titleLabel = new Label("Занятия в аудитории");
+        titleLabel.getStyleClass().add("auditorium-pair-title");
+        root.getChildren().add(titleLabel);
+        var pairs = new ArrayList<>(auditorium.getPairs());
+        pairs.sort(Comparator.comparing(Pair::getDayOfTheWeek));
+
+        int currentDay = -1;
+        for (var pair: pairs) {
+            if (pair.getDayOfTheWeek().compareTo(currentDay) != 0) {
+                Label dayLabel = new Label(DateService.daysOfWeek.get(pair.getDayOfTheWeek()));
+                dayLabel.getStyleClass().add("auditorium-pair-title");
+                root.getChildren().add(dayLabel);
+
+                currentDay = pair.getDayOfTheWeek();
+            }
+            HBox pairBox = new HBox();
+
+            VBox timeBox = new VBox();
+            Label beginLabel = new Label(pair.getClearBeginTIme().toString());
+            Label separatorLabel = new Label("|");
+            Label endLabel = new Label(pair.getClearEndTIme().toString());
+            timeBox.alignmentProperty().setValue(Pos.CENTER);
+            timeBox.getChildren().addAll(beginLabel, separatorLabel, endLabel);
+
+            Label pairNameLabel = new Label(pair.getSubject());
+            Label pairGroupLabel = new Label(pair.getGroup().getName());
+            Label pairTeacherLabel = new Label(pair.getTeacher().formatFIO());
+
+            pairNameLabel.getStyleClass().add("auditorium-pair-title");
+
+            pairBox.getChildren().addAll(timeBox, pairNameLabel, pairGroupLabel, pairTeacherLabel);
+            pairBox.getStyleClass().add("auditorium-pair-pane");
+            pairBox.alignmentProperty().setValue(Pos.CENTER_LEFT);
+
+            root.getChildren().add(pairBox);
+        }
+
+        return root;
     }
 
     @Nullable
